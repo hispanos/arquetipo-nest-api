aea8be5308514f1784919b22b5e347429800f2e9
Mailer Martinez <mailermartinez@gmail.com>
2025-12-03 13:01:40 -0500

    feat: add cocumentation with scalar

--- src/api/example/example.controller.ts
+++ src/api/example/example.controller.ts
@@ -2,32 +2,49 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ExampleService } from './example.service';
 import { CreateExampleDto } from './dto/create-example.dto';
 import { UpdateExampleDto } from './dto/update-example.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('example')
+@ApiTags('example')
 export class ExampleController {
   constructor(private readonly exampleService: ExampleService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Crear un ejemplo' })
+  @ApiResponse({ status: 201, description: 'El ejemplo ha sido creado correctamente' })
+  @ApiResponse({ status: 400, description: 'Error al crear el ejemplo' })
   create(@Body() createExampleDto: CreateExampleDto) {
     return this.exampleService.create(createExampleDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Obtener todos los ejemplos' })
+  @ApiResponse({ status: 200, description: 'Los ejemplos han sido obtenidos correctamente' })
+  @ApiResponse({ status: 400, description: 'Error al obtener los ejemplos' })
   findAll() {
     return this.exampleService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener un ejemplo por su ID' })
+  @ApiResponse({ status: 200, description: 'El ejemplo ha sido obtenido correctamente' })
+  @ApiResponse({ status: 400, description: 'Error al obtener el ejemplo' })
   findOne(@Param('id') id: string) {
     return this.exampleService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Actualizar un ejemplo por su ID' })
+  @ApiResponse({ status: 200, description: 'El ejemplo ha sido actualizado correctamente' })
+  @ApiResponse({ status: 400, description: 'Error al actualizar el ejemplo' })
   update(@Param('id') id: string, @Body() updateExampleDto: UpdateExampleDto) {
     return this.exampleService.update(+id, updateExampleDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar un ejemplo por su ID' })
+  @ApiResponse({ status: 200, description: 'El ejemplo ha sido eliminado correctamente' })
+  @ApiResponse({ status: 400, description: 'Error al eliminar el ejemplo' })
   remove(@Param('id') id: string) {
     return this.exampleService.remove(+id);
   }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { WrapResponseInterceptor } from './common/interceptors/wrap-response.int
 import { initializeErrsole } from './config/errsole.config';
 import errsole from 'errsole';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 initializeErrsole();
 
@@ -23,6 +25,21 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new ClassSerializerInterceptor(app.get('Reflector')),
   );
+
+  const config = new DocumentBuilder()
+  .setTitle('API de Facturación electrónica DIAN Modern')
+  .setDescription('API de Facturación electrónica DIAN, para el manejo de facturas electrónicas')
+  .setVersion('1.0')
+  .addTag('dian-modern')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  app.use(
+    '/docs',
+    apiReference({
+      content: document,
+      theme: 'deepSpace'
+    }),
+  )
 
   app.enableCors();
 
